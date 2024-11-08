66e5aa07333a49ae5e37ccd65daea7fb59a7312e
Sky Blue <dawadiaakash@gmail.com>
2024-11-08 12:27:09 +0545

    feat: load data to ui from server/api

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
+<script setup>
+    const {data} = await useFetch('/api/learn')
+</script>
+
 <template>
     <div>
         <h1>
             Playing with Nuxt-Server. 
         </h1>
+        <p>
+            Json Data from server is: {{ data }}
+        </p>
     </div>
 </template>
